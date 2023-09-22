0906b64dec693f1829fb665021a0cbc67cabfa89
Hozefa Bhanpurawala <Hozefa.Bhanpurawala@lionbridge.com>
2023-09-22 15:19:51 +0530

    updated add order code

--- TranslationAPI/Model/Order.cs
+++ TranslationAPI/Model/Order.cs
@@ -1,12 +1,12 @@
 ﻿public class Order
 {
     public decimal OrderId { get; set; }
-    public string OrderName { get; set; }
-    public string InputFileURL { get; set; }
-    public string OutputFileURL { get; set; }
+    public string? OrderName { get; set; }
+    public string? InputFileURL { get; set; }
+    public string? OutputFileURL { get; set; }
     public short StatusId { get; set; }
     public DateTime SubmissionDate { get; set; }
-    public DateTime CompletedDate { get; set; }
+    public DateTime? CompletedDate { get; set; }
 }
 
 public enum Status
